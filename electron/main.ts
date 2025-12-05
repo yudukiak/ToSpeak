2991fa0e33b45d470fb09a5bdaadbeb384f50b47
紅坂柚月 <akasaka@ydk.vc>
2025-12-05 21:10:50 +0900

    [update] アイコンの追加と反映

--- electron/main.ts
+++ electron/main.ts
@@ -34,7 +34,7 @@ const storedLogs: ToastLog[] = []
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
